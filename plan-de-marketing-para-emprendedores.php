89081d90d0665cea8aedeadb6c7192fa4b837b1a
Richard Maya <richard.maya94@gmail.com>
2020-04-14 19:22:25 -0500

    Landing Marketing Emprendedores

---
+++ plan-de-marketing-para-emprendedores.php
@@ -0,0 +1,369 @@
+<!doctype html>
+<html lang="es-MX">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="robots" content="index, follow">
+    <meta name="bingbot" content="index, follow">
+    <meta name="googlebot" content="index, follow">
+    <meta name="copyright" content="Copyright © 2020 Iconica Studio">
+    <meta name="p:domain_verify" content="e3eb296082b65a37cf31940943bcf7b9" />
+	<link rel="shortcut icon" href="favicon.ico">
+
+    <!-- GOOGLE TAG MANAGER
+    ================================================== -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KKLXD39');</script>
+	
+	<!-- Meta tags -->
+    <link rel="canonical" href="https://iconica.mx/plan-de-marketing-para-emprendedores.php">
+    <title>Plan de Marketing para Emprendedores | Iconica Studio</title>
+    <meta name="description" content="Plan de Marketing para Emprendedores. Crece tu negocio gracias a las redes sociales.">
+    <meta name="author" content="Iconica Studio">
+
+    <!-- OpenGraph -->
+    <meta property="og:type" content="business.business">
+    <meta property="og:title" content="Plan de Marketing para Emprendedores | Iconica Studio">
+    <meta property="og:url" content="https://iconica.mx/plan-de-marketing-para-emprendedores.php">
+    <meta property="og:image" content="https://iconica.mx/assets/ui/iconica-banner.jpg">
+    <meta property="business:contact_data:street_address" content="Av. Benito Juárez Norte No. 907">
+    <meta property="business:contact_data:locality" content="Metepec">
+    <meta property="business:contact_data:region" content="México">
+    <meta property="business:contact_data:postal_code" content="52169">
+    <meta property="business:contact_data:country_name" content="Mexico">
+
+    <!-- Twitter -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@iconicamx">
+    <meta name="twitter:title" content="Plan de Marketing para Emprendedores | Iconica Studio">
+    <meta name="twitter:description" content="Plan de Marketing para Emprendedores. Crece tu negocio gracias a las redes sociales.">
+    <meta name="twitter:image" content="https://iconica.mx/assets/ui/iconica-banner.jpg">
+
+    <!-- Schema -->
+    <script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "Webpage",
+        "url": "https://iconica.mx/plan-de-marketing-para-emprendedores.php",
+        "name": "Iconica Studio",
+        "headline": "Plan de Marketing para Emprendedores",
+        "description": "Plan de Marketing para Emprendedores. Crece tu negocio gracias a las redes sociales.",
+        "mainEntityOfPage": {
+        "@type": "WebPage",
+        "@id": "https://iconica.mx/"
+        },
+        "publisher": {
+        "@type": "Organization",
+        "name": "Iconica Studio",
+        "logo": {
+            "url": "https://iconica.mx/assets/ui/iconica-studio-diseno-grafico-marketing-digital.png",
+            "width": 900,
+            "height": 323,
+            "@type": "ImageObject"
+        }
+        },
+        "image": {
+        "@type": "ImageObject",
+        "url": "https://iconica.mx/assets/ui/iconica-banner.jpg",
+        "width": 820,
+        "height": 462
+        }
+    }
+    </script>
+	
+	<!-- STYLESHEETS
+    ================================================== -->
+    <link href="assets/css/sub-main.css" rel="stylesheet">
+    <link href="assets/css/plan-marketing-emprendedores.css" rel="stylesheet">
+
+    <?php 
+        $url = $_SERVER['REQUEST_URI']; 
+
+        $backgrounds = [
+            "test-1-webp" => "assets/img/landings/plan-marketing-emprendedores-1.webp",
+            "test-1-jpg" => "assets/img/landings/plan-marketing-emprendedores-1.jpg",
+            "test-2-webp" => "assets/img/landings/plan-marketing-emprendedores-2.webp",
+            "test-2-jpg" => "assets/img/landings/plan-marketing-emprendedores-2.jpg"
+        ];
+    ?>
+
+    <style>
+        section.intro{ 
+            background-image: url("assets/img/landings/plan-marketing-emprendedores-1.jpg");
+            background-image: url(
+            <?php
+                if (strpos($url, 'bg=1') !== false){
+                    echo $backgrounds['test-1-jpg'];
+                }
+                if (strpos($url, 'bg=2') !== false) {
+                    echo $backgrounds['test-2-jpg'];
+                }
+            ?>);
+            background-image: url(
+            <?php
+                if (strpos($url, 'bg=1') !== false){
+                    echo $backgrounds['test-1-webp'];
+                }
+                if (strpos($url, 'bg=2') !== false) {
+                    echo $backgrounds['test-2-webp'];
+                }
+            ?>);
+        }
+    </style>
+</head>
+<body>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KKLXD39" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
+<!-- NAVBAR
+================================================== -->
+<nav class="navbar w3-top w3-card">
+    <div class="w3-bar w3-large">
+        <div class="w3-container">
+            <div class="w3-row">
+                <div class="w3-col s6">
+                    <a href="/" class="w3-bar-item w3-button menu-logo">
+                        <picture>
+                            <source srcset="assets/img/home/iconica-studio-diseno-grafico-imagen-corporativa-videos-paginas-web.webp" type="image/webp" alt="iconica-studio-logo" width="112" height="40">
+                            <source srcset="assets/img/home/iconica-studio-diseno-grafico-imagen-corporativa-videos-paginas-web.png" type="image/png" alt="iconica-studio-logo" width="112" height="40">
+                            <img src="assets/img/home/iconica-studio-diseno-grafico-imagen-corporativa-videos-paginas-web.png" alt="iconica-studio-logo" width="112" height="40">
+                        </picture>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
+<div class="w3-bar w3-large navbar-mobile w3-top w3-card">
+    <a href="/" class="w3-bar-item w3-button menu-logo" title="iconica-studio-diseno-grafico">
+        <picture>
+            <source srcset="assets/ui/menu-logo.webp" type="image/webp" alt="iconica-studio-logo" width="40" height="40">
+            <source srcset="assets/ui/menu-logo.png" type="image/png" alt="iconica-studio-logo" width="40" height="40">
+            <img src="assets/ui/menu-logo.png" alt="iconica-studio-logo" width="40" height="40">
+        </picture>
+    </a>
+</div>
+
+
+<main>
+<!-- INTRO
+================================================== -->
+<section class="intro" id="descargar">
+    <div class="w3-row">
+        <div class="w3-col s12 m6"><p></p></div>
+        <div class="w3-col s12 m6 w3-theme-l5 h-100 left-side">
+            <div class="w3-padding-large">
+                <div class="w3-row w3-margin-top">
+                    <div class="w3-col s12 w3-margin-top">
+                        <header class="w3-margin-top">
+                            <h1 class="w3-text-theme w3-margin-top">Plan de Marketing para Emprendedores</h1>
+                            <h5>Crece tu negocio con las redes sociales</h5>
+                        </header>
+                        <p>Déjanos tus datos y enviaremos a tu correo más información sobre nuestro plan de marketing y <b>cómo podemos ayudarte a incrementar tus ventas.</b></p>
+                        <div class="w3-row">
+                            <div class="w3-col s12">
+                                <iframe frameborder="0" style="height:620px;width:99%;border:none;overflow:hidden;" src='https://forms.zohopublic.com/iconicastudio/form/Marketingparaemprendedores/formperma/kD40FSsXp4piGnm--izU9PJU6i2CU9kY2rhNukPeth0'></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- WHO IS THIS FOR?
+================================================== -->
+<section class="w3-theme-d1">
+    <div class="w3-container w3-padding-large">
+        <div class="w3-row w3-padding-large">
+            <div class="w3-col s12 m3"><p></p></div>
+            <div class="w3-col s12 m6 w3-center">
+                <h4>El plan ideal para negocios que van empezando o que quieren impulsar sus ventas a través de las redes sociales.</h4>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- SNEEK PEAK
+================================================== -->
+<section class="w3-section w3-padding-36 for-who">
+    <div class="w3-container w3-padding-large">
+        <div class="w3-row">
+            <div class="w3-col s12 w3-center">
+                <h3>¿Qué incluye el plan?</h3>
+            </div>
+        </div>
+        <div class="w3-row-padding w3-padding-large">
+            <div class="w3-row w3-center">
+                <div class="w3-col s12 m6">
+                    <ul class="w3-ul">
+                        <li>Creación de estrategia de marketing digital</li>
+                        <li>Configuración o actualización de cuentas sociales</li>
+                        <li>Diseño y publicación de anuncios pagados</li>
+                    </ul>
+                </div>
+                <div class="w3-col s12 m6">
+                    <ul class="w3-ul">
+                        <li>Alcance mínimo de 20,000 personas al mes</li>
+                        <li>Monitoreo y optimización de anuncios</li>
+                        <li>Reportes mensuales de resultados</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="w3-row w3-padding-large">
+            <div class="w3-col s12 w3-center">
+                <a href="#descargar" title="plan-de-marketing-para-emprendedores">
+                    <button class="w3-btn w3-theme w3-xlarge" role="button">Solicitar más información</button>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- SOCIAL PROOF
+================================================== -->
+<section class="w3-padding-32 social-proof">
+    <div class="w3-container">
+        <div class="w3-row w3-center">
+            <h4>Marcas que confían en nosotros</h4>
+        </div>
+        <div class="w3-row">
+            <div class="w3-col s12 m1 l2"><p></p></div>
+            <div class="w3-col s12 m10 l8">
+                <div class="w3-row-padding">
+                    <div class="w3-col s6 m2">
+                        <picture>
+                            <source srcset="assets/img/home/customers/app-escolar.webp" type="image/webp" alt="app-escolar-logo" class="w3-image">
+                            <source srcset="assets/img/home/customers/app-escolar.png" type="image/png" alt="app-escolar-logo" class="w3-image">
+                            <img src="assets/img/home/customers/app-escolar.png" alt="app-escolar-logo" class="w3-image">
+                        </picture>
+                    </div>
+                    <div class="w3-col s6 m2">
+                        <picture>
+                            <source srcset="assets/img/home/customers/fundidora.webp" type="image/webp" alt="fundidora-house-taco-logo" class="w3-image">
+                            <source srcset="assets/img/home/customers/fundidora.png" type="image/png" alt="fundidora-house-taco-logo" class="w3-image">
+                            <img src="assets/img/home/customers/fundidora.png" alt="fundidora-house-taco-logo" class="w3-image">
+                        </picture>
+                    </div>
+                    <div class="w3-col s6 m2">
+                        <picture>
+                            <source srcset="assets/img/home/customers/cosecha-natural.webp" type="image/webp" alt="cosecha-natural-logo" class="w3-image">
+                            <source srcset="assets/img/home/customers/cosecha-natural.png" type="image/png" alt="cosecha-natural-logo" class="w3-image">
+                            <img src="assets/img/home/customers/cosecha-natural.png" alt="cosecha-natural-logo" class="w3-image">
+                        </picture>
+                    </div>
+                    <div class="w3-col s6 m2">
+                        <picture>
+                            <source srcset="assets/img/home/customers/grupo-rh1.webp" type="image/webp" alt="grupo-rh1-logo" class="w3-image">
+                            <source srcset="assets/img/home/customers/grupo-rh1.png" type="image/png" alt="grupo-rh1-logo" class="w3-image">
+                            <img src="assets/img/home/customers/grupo-rh1.png" alt="grupo-rh1-logo" class="w3-image">
+                        </picture>
+                    </div>
+                    <div class="w3-col s6 m2">
+                        <picture>
+                            <source srcset="assets/img/home/customers/helados-americanos.webp" type="image/webp" alt="helados-americanos-logo" class="w3-image">
+                            <source srcset="assets/img/home/customers/helados-americanos.png" type="image/png" alt="helados-americanos-logo" class="w3-image">
+                            <img src="assets/img/home/customers/helados-americanos.png" alt="helados-americanos-logo" class="w3-image">
+                        </picture>
+                    </div>
+                    <div class="w3-col s6 m2">
+                        <picture>
+                            <source srcset="assets/img/home/customers/balbek.webp" type="image/webp" alt="balbek-logo" class="w3-image">
+                            <source srcset="assets/img/home/customers/balbek.png" type="image/png" alt="balbek-logo" class="w3-image">
+                            <img src="assets/img/home/customers/balbek.png" alt="balbek-logo" class="w3-image">
+                        </picture>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</main>
+
+
+<!-- FOOTER
+================================================== -->
+<footer class="w3-container w3-theme-dark">
+    <div class="w3-container">
+        <div class="w3-row-padding">
+            <div class="w3-col m6">
+                <p class="w3-medium"><span class="semplicita-font">&copy;</span> 2020 ICONICA STUDIO. Todos los derechos reservados.</p>
+            </div>
+            <div class="w3-col m6 w3-right-align">
+                <p class="w3-medium"><a href="aviso-privacidad.html" title="aviso-privacidad-iconica-studio" class="px-1">Aviso de Privacidad</a></p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+<!-- MICRODATA
+================================================== -->
+<div itemscope itemtype="https://schema.org/Service">
+    <meta itemprop="serviceType" content="Plan de Marketing para Emprendedores" />
+    <span itemprop="provider" itemscope itemtype="https://schema.org/LocalBusiness">
+        <span itemprop="name">Iconica Studio</span>
+        <div itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
+            <meta itemprop="height" content="820">
+            <meta itemprop="width" content="462">
+            <meta itemprop="url" content="https://iconica.mx/assets/ui/iconica-banner.jpg">
+            <amp-img alt="iconica-studio" width="820" height="462" layout="intrinsic" src="https://iconica.mx/assets/ui/iconica-banner.webp">
+                <amp-img alt="iconica-studio" fallback width="820" height="462" layout="intrinsic" src="https://iconica.mx/assets/ui/iconica-banner.jpg"></amp-img>
+            </amp-img>
+        </div>
+        <span itemprop="telephone">7226560428</span>
+        <span itemprop="priceRange">$$</span>
+        <div itemprop="address" itemscope itemtype="https://schema.org/PostalAddress">
+            <span itemprop="streetAddress">Av. Benito Juárez Norte No. 907</span>
+            <span itemprop="addressLocality">Metepec</span>
+            <span itemprop="addressRegion">México</span>
+            <span itemprop="postalCode">52169</span>
+        </div>
+    </span>
+    <span itemprop="areaServed" itemscope itemtype="https://schema.org/State">
+        <span itemprop="name">Estado de México</span>
+    </span>
+    <ul itemprop="hasOfferCatalog" itemscope itemtype="https://schema.org/OfferCatalog">
+        <li itemprop="itemListElement" itemscope itemtype="https://schema.org/OfferCatalog">
+            <span itemprop="name">Plan de Marketing para Emprendedores</span>
+            <ul itemprop="itemListElement" itemscope itemtype="https://schema.org/OfferCatalog">
+                <li itemprop="itemListElement" itemscope itemtype="https://schema.org/Offer">
+                    <div itemprop="itemOffered" itemscope itemtype="https://schema.org/Service">
+                        <span itemprop="name">Publicidad en redes sociales</span>
+                    </div>
+                </li>
+                <li itemprop="itemListElement" itemscope itemtype="https://schema.org/Offer">
+                    <div itemprop="itemOffered" itemscope itemtype="https://schema.org/Service">
+                        <span itemprop="name">Publicidad en Facebook e Instagram</span>
+                    </div>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</div>
+    
+
+<!-- MICROFORMATS
+================================================== -->
+<ul class="hproduct">
+    <li class="brand">Iconica Studio</li>
+    <li class="category">Service</li>
+    <li class="fn">Plan de Marketing para Emprendedores</li>
+    <li class="description">Plan de Marketing para Emprendedores. Crece tu negocio gracias a las redes sociales.</li>
+    <li class="url">https://iconica.mx/plan-de-marketing-para-emprendedores.php</li>
+</ul>
+
+
+<!-- SCRIPTS
+================================================== -->
+<script src="assets/js/jquery.min.js"></script>
+</body>
+</html>
